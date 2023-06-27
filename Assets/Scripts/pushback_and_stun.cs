294ed78709079b2a759e11a5f7c5621b86854cf5
arshiabehzad <behzad@usc.edu>
2023-06-27 01:35:45 -0700

    added block that requires screech ability to move
    - allows plater to go to third room

--- Assets/Scripts/pushback_and_stun.cs
+++ Assets/Scripts/pushback_and_stun.cs
@@ -8,6 +8,7 @@ public class pushback_and_stun : MonoBehaviour
     public float pushForce = 200f;
 
     private GameObject shockWave;
+    private GameObject shockWavePlayer;
     private int counter = 0;
     private int pushTime = 2;
     PlayerMovement playerMovement;
@@ -28,22 +29,30 @@ public class pushback_and_stun : MonoBehaviour
             {
                 playerController.TakeDamage(0.5f);
                 playerMovement.enabled = false;
-            } 
+            }
+            StartCoroutine(Unstun());
+        } else if (other.gameObject == shockWavePlayer)
+        {
+            if (gameObject.CompareTag("ScreechBlock"))
+            {
+                
+            }
             else if (gameObject.CompareTag("Enemy"))
             {
                 print("works");
                 //TODO: add once rock enemy 
             }
-
-            StartCoroutine(Unstun());
         }
     }
 
     private void OnTriggerStay2D(Collider2D other)
     {
         shockWave = GameObject.FindGameObjectWithTag(objectTag);
-        if (other.gameObject == shockWave)
+        shockWavePlayer = GameObject.FindGameObjectWithTag("ShockwavePlayer");
+        
+        if (other.gameObject == shockWave && !gameObject.CompareTag("ScreechBlock") )
         {
+            print("doesnt work");
             Vector2 dir = other.transform.position - transform.position;
             // We then get the opposite (-Vector3) and normalize it
             dir = -dir.normalized;
@@ -52,6 +61,21 @@ public class pushback_and_stun : MonoBehaviour
                 GetComponent<Rigidbody2D>().velocity = Vector2.zero;
                 GetComponent<Rigidbody2D>().AddForce(dir*pushForce);
             }
+        } 
+        if (gameObject.CompareTag("ScreechBlock"))
+        {
+            if (other.gameObject == shockWavePlayer)
+            {
+                print("works well");
+                Vector2 dir = other.transform.position - transform.position;
+                // We then get the opposite (-Vector3) and normalize it
+                dir = -dir.normalized;
+                if (GetComponent<Rigidbody2D>() != null)
+                {
+                    GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                    GetComponent<Rigidbody2D>().AddForce(dir*pushForce);
+                }
+            }   
         }
     }
 
