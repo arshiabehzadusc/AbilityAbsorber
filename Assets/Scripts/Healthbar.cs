a5049221aeabe21332bd64f8b6aad681a3e74548
arshiabehzad <behzad@usc.edu>
2023-07-08 12:10:18 -0700

    Different Health for When Bat
    -added a system to easily adjust health of player dependent on what ability they are using at the time
    - edits to the code and the UI slider

--- Assets/Scripts/Healthbar.cs
+++ Assets/Scripts/Healthbar.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,10 +9,46 @@ public class Healthbar : MonoBehaviour
     public Slider slider;
     public PlayerController playerHealth;
 
+    private string ability;
     // Update is called once per frame
+
+    private void Start()
+    {
+        ability = "none";
+    }
+
     void Update()
     {
-        slider.value = playerHealth.health;
+       
+        if (ability == "bat")
+        {
+            slider.value = playerHealth.batHealth;
+        } else 
+        {
+            slider.value = playerHealth.health;
+        }
+    }
+
+    public void setHealthBar(string ability)
+    {
+        this.ability = ability;
+        print("setHealthBar");
+        if (ability == "bat")
+        {
+            slider.maxValue = 2;
+            RectTransform backgroundRT = slider.transform.Find("Background").GetComponent<RectTransform>();
+            RectTransform fillAreaRT = slider.transform.Find("Fill Area").GetComponent<RectTransform>();
+            backgroundRT.offsetMax = new Vector2(-75, backgroundRT.offsetMax.y); // -50 is the new "right" value, keep the original "top" value
+            fillAreaRT.offsetMax = new Vector2(-75, fillAreaRT.offsetMax.y);
+        }
+        else
+        {
+            slider.maxValue = 7;
+            RectTransform backgroundRT = slider.transform.Find("Background").GetComponent<RectTransform>();
+            RectTransform fillAreaRT = slider.transform.Find("Fill Area").GetComponent<RectTransform>();
+            backgroundRT.offsetMax = new Vector2(0, backgroundRT.offsetMax.y); // -50 is the new "right" value, keep the original "top" value
+            fillAreaRT.offsetMax = new Vector2(0, fillAreaRT.offsetMax.y);
+        }
     }
 }
 
