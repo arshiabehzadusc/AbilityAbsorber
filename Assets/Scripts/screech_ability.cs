1d96beddb24c325c76994a33e1d2927af645d16c
Sameer Channar <schannar@asu.edu>
2023-07-08 18:25:50 -0700

    Added mega screech which is only big if passing through megaphone

--- Assets/Scripts/screech_ability.cs
+++ Assets/Scripts/screech_ability.cs
@@ -10,6 +10,13 @@ public class screech_ability : MonoBehaviour
     public GameObject shockWave;
     public Transform shockWaveSpawn;
 
+    private bool isMegaScreech = false;
+    float regularWidth = 3f;
+    float regularHeight = 3f;
+    float megaWidth = 8f;
+    float megaHeight = 7f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +31,7 @@ public class screech_ability : MonoBehaviour
         {
             if (waveObject == null)
             {
+                isMegaScreech = false;
                 Vector3 spawnPosition = shockWaveSpawn.position;
                 waveObject = Instantiate(shockWave, spawnPosition, Quaternion.identity);
             }
@@ -31,18 +39,57 @@ public class screech_ability : MonoBehaviour
 
         if (waveObject != null)
         {
-            waveObject.transform.position = shockWaveSpawn.position;
+            isMegaScreech = false;
+            
+            // if the shockwave is going through the Megaphone, enlarge it, set MegaScreech=true
+            // now it can shatter rock
+            GameObject[] megaphones = GameObject.FindGameObjectsWithTag("Megaphone");
+            foreach (GameObject megaphone in megaphones)
+            {
+                float distance = Vector3.Distance(waveObject.transform.position, megaphone.transform.position);
+                if (distance <= 3)
+                {
+                    print("player shockwave is near megaphone");
+                    isMegaScreech = true;
+                    break;
+                }
+            }
+            float newWidth;
+            float newHeight;
+            Vector3 offset; 
+            if (isMegaScreech) {
+                newWidth = megaWidth;
+                newHeight = megaHeight;
+                offset = new Vector3(3f, 0f, 0f);
+            }
+            else {
+                newWidth = regularWidth;
+                newHeight = regularHeight;
+                offset = new Vector3(0f, 0f, 0f);
+            }
+
+
+            
             if (!playerMovement.isMovingLeft)
             {
+                waveObject.transform.position = shockWaveSpawn.position + offset;
+
                 // Flip the sprite to face right
-                waveObject.transform.localScale = new Vector3(3f, 3f, 1f);
+                waveObject.transform.localScale = new Vector3(newWidth, newHeight, 1f);
             }
             else
             {
+                waveObject.transform.position = shockWaveSpawn.position + (-1 * offset);
+
                 // Flip the sprite to face left
-                waveObject.transform.localScale = new Vector3(-3f, 3f, 1f);
+                waveObject.transform.localScale = new Vector3(-1f * newWidth, newHeight, 1f);
             }
+
             Destroy(waveObject, 3f);
         }
+    }
+
+    public bool getIsMegaScreech() {
+        return isMegaScreech;
     }
 }
