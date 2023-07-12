7a03e85265ea9631c82770cfb41830c6d42fe1fb
Sameer Channar <schannar@asu.edu>
2023-07-11 19:39:27 -0700

    Fixed generator color

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -1,32 +1,64 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class Generator : MonoBehaviour
+// {
+//     // Start is called before the first frame update
+
+//     private SpriteRenderer renderer;
+
+//     void Start()
+//     {
+//         renderer = GetComponent<SpriteRenderer>();
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+        
+//     }
+
+//     // touching the actual magnet
+//     private void OnTriggerEnter2D(Collider2D other) 
+//     {
+//         if (other.gameObject.CompareTag("ElectricAbility"))
+//         {
+//             print("here");
+//             Color color = HexToColor("E5C1C1");
+//             renderer.material.color = color;
+//         }
+//     }
+
+//     private Color HexToColor(string hex)
+//     {
+//         Color color = Color.black;
+//         ColorUtility.TryParseHtmlString("#" + hex, out color);
+//         return color;
+//     }
+// }
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Generator : MonoBehaviour
+public class Generator : MonoBehaviour 
 {
-    // Start is called before the first frame update
-
     private SpriteRenderer renderer;
 
-    void Start()
+    void Start() 
     {
         renderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    // touching the actual magnet
-    private void OnTriggerEnter2D(Collider2D other) 
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("ElectricAbility"))
         {
             print("here");
-            Color color = HexToColor("FFFFFF");
-            renderer.material.color = color;
+            Color color = HexToColor("E5C1C1");
+            renderer.color = color;  // Change here
         }
     }
 
