fcb25b3cf51a0921eb534f2bdb89dcf6c03eb8ab
arshiabehzad <behzad@usc.edu>
2023-07-12 14:01:34 -0700

    Room 4 of Level 3 Complete
    - When gate opens water comes out
    - Water Kills Fire Enemy
    - Water fades away (drys out) after a few seconds
    - Player cannot push generator (must use magnet)
    - Bug Fixes

--- Assets/Scripts/Gate.cs
+++ Assets/Scripts/Gate.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class Gate : MonoBehaviour
 {
-    private Animator animator; 
-
+    private Animator animator;
+    public GameObject water;
+    private SpriteRenderer waterSR;
+    public GameObject fireEnemy;
+    public float growTime = 2f;  // Total time it takes to grow
+    public float dryTime = 2f;
+    private Vector3 initialScale;
+    private Vector3 targetScale;
+    
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        initialScale = water.transform.localScale;
+        targetScale = new Vector2(1.6f,3.2f);  // Replace with your desired scale
+        GameObject waterChild = water.transform.GetChild(0).gameObject; // assuming parentTransform has at least one child
+        waterSR = waterChild.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -21,5 +32,37 @@ public class Gate : MonoBehaviour
     public void openGate()
     {
         animator.SetBool("genTurnedOn", true);
+        StartCoroutine(ReleaseWater());
+        Destroy(fireEnemy, 2f);
+    }
+    
+    IEnumerator ReleaseWater()
+    {
+        yield return new WaitForSeconds(1f);
+        float elapsedTime = 0f;
+
+        while (elapsedTime < growTime)
+        {
+            water.transform.localScale = Vector3.Lerp(initialScale, targetScale, (elapsedTime / growTime));
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+        // Ensure it gets exactly to the target
+        water.transform.localScale = targetScale;
+        
+        yield return new WaitForSeconds(1f);
+        Color startColor = waterSR.color;
+        elapsedTime = 0f;
+
+        while (elapsedTime < dryTime)
+        {
+            elapsedTime += Time.deltaTime;
+            print(elapsedTime);
+            float alpha = Mathf.Lerp(1f, 0f, elapsedTime / dryTime);
+            waterSR.color = new Color(startColor.r, startColor.g, startColor.b, alpha);
+            yield return null;
+        }
+        Destroy(water);
     }
 }
