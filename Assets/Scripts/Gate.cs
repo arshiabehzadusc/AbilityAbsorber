221b85d612a9f9ba112c86a19f9dc261a5bbd128
arshiabehzad <behzad@usc.edu>
2023-07-11 19:37:14 -0700

    Gate Animation
    -added animation controller and script for gate opening

---
+++ Assets/Scripts/Gate.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    private Animator animator; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void openGate()
+    {
+        animator.SetBool("genTurnedOn", true);
+    }
+}
