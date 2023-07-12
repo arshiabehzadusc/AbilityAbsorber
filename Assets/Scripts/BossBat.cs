fce4e02c1db975fe8695d4f68fa374df7d9fe54e
justin jenish <jenish@usc.edu>
2023-07-11 19:28:13 -0700

    fixes

--- Assets/Scripts/BossBat.cs
+++ Assets/Scripts/BossBat.cs
@@ -6,8 +6,11 @@ public class BossBat : MonoBehaviour
 {
     public GameObject Torch1;
     public GameObject Torch2;
+    public GameObject puzzleBanner;
 
     private Bat bossBat;
+
+    private bool isActionPerformed = false;
 
     // Start is called before the first frame update
     void Start()
@@ -18,10 +21,13 @@ public class BossBat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (bossBat.getIsCorpse()) {
+        if (bossBat.getIsCorpse() && !isActionPerformed) {
+
             Torch1.SetActive(true);
             Torch2.SetActive(true);
-
+            puzzleBanner.SetActive(true);
+            isActionPerformed = true;
+            Time.timeScale = 0;
         }
     }
 }
