5b2a58ce75db226b3ad21eceee7868806ea9c038
Sameer Channar <schannar@asu.edu>
2023-07-11 18:59:12 -0700

    Generator

---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Renderer renderer;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // touching the actual magnet
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.gameObject.CompareTag("ElectricAbility"))
+        {
+            Color color = HexToColor("FFFFFF");
+            renderer.material.color = color;
+        }
+    }
+
+    private Color HexToColor(string hex)
+    {
+        Color color = Color.black;
+        ColorUtility.TryParseHtmlString("#" + hex, out color);
+        return color;
+    }
+}
