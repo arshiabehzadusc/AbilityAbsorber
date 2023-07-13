61d8479c5ce620e6e9d014c7ba3e79adb89a0bc7
Sameer Channar <schannar@asu.edu>
2023-07-12 19:57:06 -0700

    fixed magnet dealing damage when dead

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -64,18 +64,16 @@ public class PlayerController : MonoBehaviour
             Debug.Log("Player dealt damage to ghost by ramming");
             ghostEnemy.TakeDamage(1.0f);
         }
-         if (collision.gameObject.CompareTag("Diamond")  &&  abilityManager.getSelectedAbility() == "ram" && rockAbility.isRushing == true)
+        if (collision.gameObject.CompareTag("Diamond")  &&  abilityManager.getSelectedAbility() == "ram" && rockAbility.isRushing == true)
         {    
             Debug.Log("Player took damage by ramming into diamond");
             TakeDamage(0.5f,"Diamond");
         }
-        
-        
         if (collision.gameObject.CompareTag("SpinningHazard")) {
             TakeDamage(3f, "SpinningHazard");
         }
         if (collision.gameObject.CompareTag("Magnet")) {
-            if (abilityManager.getSelectedAbility() == "electric") {
+            if (abilityManager.getSelectedAbility() == "electric" && collision.gameObject.GetComponent<MagnetEnemy>().getIsAlive() == true) {
                 TakeDamage(3f, "Magnet");
             }
         }
